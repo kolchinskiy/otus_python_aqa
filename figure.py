13073f0f1664e49d9cf6ce9f65e61c960bfbb900
kolchinskiy <kolchinskiy.a@gmail.com>
2023-06-09 02:09:37 +0400

    homework 3

--- figure.py
+++
@@ -1,9 +0,0 @@
-class Figure:
-
-    def add_area(self, figure):
-        if isinstance(figure, Figure):
-            area_1 = self.area()
-            area_2 = figure.area()
-            summa = area_1 + area_2
-            return summa
-        raise ValueError(f'Object {figure} is not subclass of Figure class')
